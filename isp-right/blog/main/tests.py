ace8529b6f2227c2d816fc8e8ba78d0c04ec29de
Lavelina <7660990@gmail.com>
2022-06-07 00:31:45 +0300

    added tests

--- isp-right/blog/main/tests.py
+++ isp-right/blog/main/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from main.models import *
 
-# Create your tests here.
+
+class URLTests(TestCase):
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_register(self):
+        response = self.client.get("/registration/")
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        login = self.client.login(username='test_user', password='testpass')
+        self.client.logout()
+
+    def test_main_view(self):
+        response = self.client.get(f'')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/index.html')
+
+    def test_section_404(self):
+        response = self.client.get("/section/66/")
+
+        self.assertEqual(response.status_code, 404)
+
